8467ce3da94969e4fa0e9b3cdfabb95cd6007b08
Nekane Ibarlucea <nknib90@gmail.com>
2022-01-20 15:31:50 +0100

    Almost

--- exercises/01-exe-js_map.js
+++ exercises/01-exe-js_map.js
@@ -1,21 +1,26 @@
 // ONLY USE THE .map() METHOD FOR THIS EXERCISE
 
-
+/*
 // Exercise 1: Capitalize each element of the array - the whole word:
 const fruits = ['pineapple', 'orange', 'mango'];
 
-let uppercaseFruits = fruits.map(() => {
-  //your code here...
+let uppercaseFruits = fruits.map((el) => {
+  return el.toUpperCase()
 })
 console.log(uppercaseFruits)
 
 // Answer should be  ['PINEALLPLE', 'ORANGE', 'MANGO'];
-
+*/
 
 // Exercise 2: Capitalize the first letter of every city. 
 const cities = ['miami', 'barcelona', 'madrid', 'amsterdam', 'berlin', 'sao paulo', 'lisbon', 'mexico city']
 
-let uppercaseCities // your code here...
+let uppercaseCities = cities.map((el)=>{
+   return el.charAt(0).toUpperCase() + el.slice(1)
+})
+
+console.log(uppercaseCities)
+// your code here...
 
 // Answer should be 
 // [ 'Miami', 'Barcelona', 'Madrid', 'Amsterdam', 'Berlin', 'Sao paulo', 'Lisbon', 'Mexico city', 'Paris' ]
@@ -23,6 +28,7 @@ let uppercaseCities // your code here...
 // Exercise  3: Create an array with only the names of the restaurants 
 // Note: Be careful here. Info is an object with a key called 'cities' which is an array
  
+/*
 const info = {
     cities : [
       {name: 'Mirazur', owner : 'Gustavo'},
@@ -38,7 +44,8 @@ const getRestaurantsNames = (arr) => {
    //your code here...
 }
 
-let restaurantsNames = getRestaurantsNames( /* pass your array here*/)
-console.log(restaurantsNames)
+let restaurantsNames = getRestaurantsNames( /* pass your array here*/
+//console.log(restaurantsNames)
 
 // Answer should be [ 'Mirazur', 'Noma', 'Gaggan', 'Geranium', 'Arpège']
+
